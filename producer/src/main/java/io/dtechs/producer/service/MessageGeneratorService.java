5d03ce0209b1779079b0fff1f1b5d02437a1c748
k4r3l1nS <makcum.kyp4ehkob@gmail.com>
2023-09-26 17:51:35 +0300

    Added quick message send method

--- producer/src/main/java/io/dtechs/producer/service/MessageGeneratorService.java
+++ producer/src/main/java/io/dtechs/producer/service/MessageGeneratorService.java
@@ -7,6 +7,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadLocalRandom;
 
 @Service
@@ -18,37 +23,97 @@ public class MessageGeneratorService {
     @Value("${kafka.topic.common}")
     private String COMMON_TOPIC;
 
-    private static final int NUMBER_OF_MESSAGES = 2000;
+    private static final int NUMBER_OF_MESSAGES = 2000000;
+
+    public void sendMessagesMultithread() {
+
+        var threadPool = Executors.newFixedThreadPool(16);
+        try {
+            var messageList = generateSizedMessages(NUMBER_OF_MESSAGES);
+            for (int index = 0; index < NUMBER_OF_MESSAGES; ++index) {
+                var messageDto = messageList.get(index);
+                int finalIndex = index;
+                CompletableFuture.supplyAsync(
+                        () -> {
+                            kafkaSender.sendTransactionalMessage(COMMON_TOPIC, messageDto);
+                            System.out.println("!!! SENDING " + (finalIndex + 1) + " MESSAGE !!!");
+                            return null;
+                        },
+                        threadPool
+                );
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
 
     public void sendMessages() {
 
         try {
-            for (int messageCount = 0; messageCount < NUMBER_OF_MESSAGES; ++messageCount) {
-                System.out.println("!!! SENDING " + (messageCount + 1) + " MESSAGE !!!");
-
-                String fileName = "file" + messageCount;
-                if (messageCount % 3 == 0) {
-                    fileName += ".mp4";
-                } else if (messageCount % 2 == 0) {
-                    fileName += ".jpg";
-                } else {
-                    fileName += ".txt";
-                }
-                File file = File.createTempFile("file" + messageCount + "_",
-                        fileName.substring(fileName.length() - 4));
-
-                var messageDto = MessageDto.builder()
-                        .file(file)
-                        .id(Math.abs(ThreadLocalRandom.current().nextLong()))
-                        .version(Math.abs(ThreadLocalRandom.current().nextLong()) % 10 == 0 ?
-                                MessageDto.Version.V2 : MessageDto.Version.V1)
-//                        .version(MessageDto.Version.V1)
-                        .build();
-
+            var messageList = generateSizedMessages(NUMBER_OF_MESSAGES);
+            for (int index = 0; index < NUMBER_OF_MESSAGES; ++index) {
+                System.out.println("!!! SENDING " + (index + 1) + " MESSAGE !!!");
+                var messageDto = messageList.get(index);
                 kafkaSender.sendTransactionalMessage(COMMON_TOPIC, messageDto);
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+    }
+
+    public List<MessageDto> generateSimpleMessages(int messageCount) {
+        List<MessageDto> messageList = new ArrayList<>();
+        try {
+            for (int index = 0; index < messageCount; ++index) {
+                String fileName = "file" + index;
+                if (index % 3 == 0) {
+                    fileName += ".mp4";
+                } else if (index % 2 == 0) {
+                    fileName += ".jpg";
+                } else {
+                    fileName += ".txt";
+                }
+                File file = File.createTempFile("file" + index + "_",
+                        fileName.substring(fileName.length() - 4));
+
+                messageList.add(
+                        MessageDto.builder()
+                                .file(file)
+                                .id(Math.abs(ThreadLocalRandom.current().nextLong()))
+                                .version(Math.abs(ThreadLocalRandom.current().nextLong()) % 10 == 0 ?
+                                        MessageDto.Version.V2 : MessageDto.Version.V1)
+                                .build()
+                );
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return messageList;
+    }
+
+    public List<MessageDto> generateSizedMessages(int messageCount) {
+        List<MessageDto> messageList = new ArrayList<>();
+        for (int index = 0; index < messageCount; ++index) {
+            String filepath = "/home/dtechs/Desktop/project/producer/src/main/resources/file_examples";
+            filepath +=
+                    (switch (Math.abs(ThreadLocalRandom.current().nextInt()) % 3) {
+                        case 0 -> "/test.mp4";
+                        case 1 -> "/test.jpg";
+                        case 2 -> "/test.txt";
+                        default -> ".err";
+                    });
+            File file = new File(filepath);
+
+            messageList.add(
+                    MessageDto.builder()
+                            .file(file)
+                            .id(Math.abs(ThreadLocalRandom.current().nextLong()))
+                            .version(Math.abs(ThreadLocalRandom.current().nextLong()) % 10 == 0 ?
+                                    MessageDto.Version.V2 : MessageDto.Version.V1)
+                    .build()
+            );
+        }
+        return messageList;
     }
 }
